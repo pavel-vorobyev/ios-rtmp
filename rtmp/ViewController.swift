8629e319d85c9b150536d11157e2571de682908b
Pavel <pavelvorobev.work@gmail.com>
2018-11-19 06:13:43 +0300

    Initial Commit

---
+++ rtmp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rtmp
+//
+//  Created by Pavel Vorobyev on 19/11/2018.
+//  Copyright © 2018 Pavel Vorobyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
